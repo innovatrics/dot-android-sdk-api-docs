bc7924d8379db7481a2637ee13602dd94ae0e7bd
root <root@runner-yg5ypvqw-project-1349-concurrent-0>
2022-08-18 14:26:43 +0000

    Face Lite 1.1.1

---
+++ face-lite/1.1.0/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":dot-face-lite:dokkaHtml/androidTestRelease":[],":dot-face-lite:dokkaHtml/debug":[],":dot-face-lite:dokkaHtml/publicApi":[],":dot-face-lite:dokkaHtml/release":[],":dot-face-lite:dokkaHtml/testFixtures":[],":dot-face-lite:dokkaHtml/testFixturesDebug":[],":dot-face-lite:dokkaHtml/testFixturesRelease":[]}'

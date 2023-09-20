b8e7b426ee416ec5d748b98bd0722dd545ab7719
Radoslav Cagala <radoslav.cagala@innovatrics.com>
2023-09-20 11:00:39 +0200

    Revert "Face Lite 6.4.0"
    This reverts commit 2388da1acb85f9d6c53e41ae7170bb4fb24236e2.

--- face-lite/6.4.0/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":dot-face-lite:dokkaHtml/androidTestRelease":[],":dot-face-lite:dokkaHtml/debug":[],":dot-face-lite:dokkaHtml/publicApi":[],":dot-face-lite:dokkaHtml/release":[],":dot-face-lite:dokkaHtml/testFixtures":[],":dot-face-lite:dokkaHtml/testFixturesDebug":[],":dot-face-lite:dokkaHtml/testFixturesRelease":[]}'

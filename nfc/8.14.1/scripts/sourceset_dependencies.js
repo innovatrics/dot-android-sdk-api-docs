403cfbf88be568ce1ac44793b9a24aabbb56157e
root <root@runner-t1lcqsgz-project-1349-concurrent-0>
2025-08-07 15:12:33 +0000

    NFC 8.14.2-test-agp-8.12.0-4

---
+++ nfc/8.14.1/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":dot-nfc:dokkaHtml/androidTestRelease":[],":dot-nfc:dokkaHtml/debug":[],":dot-nfc:dokkaHtml/publicApi":[],":dot-nfc:dokkaHtml/release":[],":dot-nfc:dokkaHtml/testFixtures":[],":dot-nfc:dokkaHtml/testFixturesDebug":[],":dot-nfc:dokkaHtml/testFixturesRelease":[]}'
